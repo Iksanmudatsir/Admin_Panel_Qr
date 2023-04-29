1873ed86427b9d6dfa64b1606d21417d38922150
A. Ichsan Mudatsir <iksan0049@gmail.com>
2023-04-29 14:19:12 +0800

    update

---
+++ src/data/tableReceipt.js
@@ -0,0 +1,29 @@
+export const orderedItems = [
+    {
+        id: 1,
+        name: "Bassang",
+        quantity: 5,
+        price:15,
+    },
+    {
+        id: 2,
+        name: "Bubur Kacang Ijo",
+        quantity: 2,
+        price: 18,
+    }
+]
+
+export const orderedItems1 = [
+    {
+        id: 1,
+        name: "Kopi Susu",
+        quantity: 5,
+        price:15,
+    },
+    {
+        id: 2,
+        name: "Sop Ubi",
+        quantity: 2,
+        price: 18,
+    }
+]
