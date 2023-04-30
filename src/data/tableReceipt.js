a92ce6ba8b6bd68a7dc1d8b148e0fdfc73b34f1c
A. Ichsan Mudatsir <iksan0049@gmail.com>
2023-05-01 05:31:21 +0800

    update

--- src/data/tableReceipt.js
+++ src/data/tableReceipt.js
@@ -1,29 +1,47 @@
 export const orderedItems = [
     {
-        id: 1,
-        name: "Bassang",
-        quantity: 5,
-        price:15,
+        costumerTable: 5,
+        items: [
+            {
+                id: 1,
+                name: "Bassang",
+                quantity: 5,
+                price: 15,
+            },
+            {
+                id: 2,
+                name: "Bubur Kacang Ijo",
+                quantity: 2,
+                price: 18,
+            },
+        ],
     },
-    {
-        id: 2,
-        name: "Bubur Kacang Ijo",
-        quantity: 2,
-        price: 18,
-    }
-]
+];
 
 export const orderedItems1 = [
     {
-        id: 1,
-        name: "Kopi Susu",
-        quantity: 5,
-        price:15,
-    },
+        costumerTable: 7,
+        items: [
+            {
+                id: 1,
+                name: "Kopi Susu",
+                quantity: 5,
+                price:15,
+            },
+            {
+                id: 2,
+                name: "Sop Ubi",
+                quantity: 2,
+                price: 18,
+            }
+        ]
+    }
+]
+
+export const orderedItems2 =[
     {
-        id: 2,
-        name: "Sop Ubi",
-        quantity: 2,
-        price: 18,
+        id: 1,
+        name: "Es Coklat",
+        
     }
 ]
