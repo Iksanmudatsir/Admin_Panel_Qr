9acd6a23722cef2374556afad5c5d73f06fdcd44
fa <zuchairul@gmail.com>
2023-06-07 02:07:30 +0800

    update home page

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 
-function Home() {
-
-
+const Home = () => {
     return (
-        <>
-
-        </>
+        <div className='flex'>
+            <div class="bg-white m-auto">
+                <div class="w-[100%] pt-5">
+                    <div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 border shadow-lg">
+                        <div class="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="text-black font-bold text-xl mb-2 leading-tight">Today Orders</div>
+                            <p class="text-grey-darker text-base">1523</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white m-auto">
+                <div class="w-[100%] pt-5">
+                    <div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 border shadow-lg">
+                        <div class="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="text-black font-bold text-xl mb-2 leading-tight">Menu Available</div>
+                            <p class="text-grey-darker text-base">31</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white m-auto">
+                <div class="w-[100%] pt-5">
+                    <div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 border shadow-lg">
+                        <div class="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="text-black font-bold text-xl mb-2 leading-tight">All Order</div>
+                            <p class="text-grey-darker text-base">1523</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white m-auto">
+                <div class="w-[100%] pt-5">
+                    <div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 border shadow-lg">
+                        <div class="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                            <div class="text-black font-bold text-xl mb-2 leading-tight">All Order</div>
+                            <p class="text-grey-darker text-base">1523</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
